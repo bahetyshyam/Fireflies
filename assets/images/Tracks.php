80241376adea96f377f47f6f50a4bcdd06746b2f
Ezihan <30643120+Ezihan@users.noreply.github.com>
2018-11-18 23:27:15 +0530

    Add files via upload

---
+++ assets/images/Tracks.php
@@ -0,0 +1,47 @@
+<?php
+	class Tracks {
+
+		private $con;
+		private $id;
+		private $num;
+		private $title;
+		private $albumId;
+		private $playlist;
+		private $duration;
+		private $path;
+
+		public function __construct($con, $id) {
+			$this->con = $con;
+			$this->id = $id;
+
+			$query = mysqli_query($this->con, "SELECT * FROM Tracks WHERE Track_id='$this->id'");
+			$track = mysqli_fetch_array($query);
+			$this->title = $track['Song'];
+			$this->albumId = $track['Album_id'];
+			$this->playlist = $track['Playlist_id'];
+			$this->duration = $track['Play_time'];
+			$this->path = $track['Song_file'];
+			$this->num = $track['Track_no'];
+		}
+
+		public function getTitle() {
+			return $this->title;
+		}
+
+		public function getAlbum() {
+			return new Album($this->con, $this->albumId);
+		}
+
+		public function getPath() {
+			return $this->path;
+		}
+
+		public function getDuration() {
+			return $this->duration;
+		}
+
+		public function getNumber() {
+			return $this->num;
+		} 
+	}
+?>
