f37a886df8cb57229133bf6bf576f3ec49074d4b
Ezihan <30643120+Ezihan@users.noreply.github.com>
2018-11-18 23:28:32 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,3 +2,14 @@ $('.sideBtn').on("click",function() {
 	$('.sideBtn').toggleClass('btnc');
 	$('.sidebar').toggleClass('side');
 });
+
+function Audio() {
+
+	this.currentlyPlaying;
+	this.audio = document.createElement('audio');
+
+	this.setTrack = function(src) {
+		this.audio.src = src;
+	}
+
+}
