4723e941d488884150db6f5538c614e9bed0fe30
Ezihan <30643120+Ezihan@users.noreply.github.com>
2018-11-20 19:46:24 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,13 +3,73 @@ $('.sideBtn').on("click",function() {
 	$('.sidebar').toggleClass('side');
 });
 
+var currentPlaylist = [];
+var tempPlaylist = [];
+var audioElement;
+var currentIndex = 0;
+ var mousedown = false;
+var repeat = false;
+
+function formatTime(seconds) {
+	var time = Math.round(seconds);
+	var minutes = Math.floor(time / 60); 
+	var seconds = time - (minutes * 60);
+
+	var extraZero = (seconds < 10) ? "0" : "";
+
+	return minutes + ":" + extraZero + seconds;
+}
+
+function updateTimeProgressBar(audio) {
+	$(".progressTime.current").text(formatTime(audio.currentTime));
+	$(".progressTime.remaining").text(formatTime(audio.duration - audio.currentTime));
+
+	var progress = audio.currentTime / audio.duration * 100;
+	$(".progressRem").css("width", progress + "%");
+}
+
+function updateVolumeProgressBar(audio) {
+	var volume = audio.volume * 100;
+	$(".volumeBar .progressRem").css("width", volume + "%");
+}
+
 function Audio() {
 
 	this.currentlyPlaying;
 	this.audio = document.createElement('audio');
 
-	this.setTrack = function(src) {
-		this.audio.src = src;
+	this.audio.addEventListener("ended", function() {
+		nextSong();
+	});
+
+	this.audio.addEventListener("canplay", function() {
+		var duration = formatTime(this.duration);
+		$(".progressTime.remaining").text(duration);
+	});
+
+	this.audio.addEventListener("timeupdate", function(){
+		if(this.duration) {
+			updateTimeProgressBar(this);
+		}
+	});
+
+	this.audio.addEventListener("volumechange", function() {
+		updateVolumeProgressBar(this);
+	});
+
+	this.setTrack = function(track) {
+		this.currentlyPlaying = track;
+		this.audio.src = track.Song_file;
 	}
 
+	this.play = function() {
+		this.audio.play();
+	}
+
+	this.pause = function() {
+		this.audio.pause();
+	}
+	this.setTime = function(seconds) {
+		this.audio.currentTime = seconds;
+	}
 }
